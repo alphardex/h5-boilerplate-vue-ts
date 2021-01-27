c13136910b1d03109ae4c701aeea642e32eb7f64
alphardex <2582347430@qq.com>
2021-01-27 17:06:02 +0800

    update

--- src/hooks/useLazy.ts
+++ src/hooks/useLazy.ts
@@ -4,9 +4,15 @@ import LazyLoad from "lazyload";
 export default () => {
   const lazyload = (imgs: NodeListOf<Element>) => {
     // @ts-ignore
-    new LazyLoad(imgs, {
-      rootMargin: "0px 0px -100px",
-    });
+    if (!navigator.userAgent.includes('WindowsWechat')) {
+      new LazyLoad(imgs, {
+        rootMargin: "0px 0px -100px",
+      });
+    } else {
+      imgs.forEach((img) => {
+        (img as HTMLImageElement).src = (img as HTMLImageElement).dataset.src!;
+      });
+    }
   };
   return {
     lazyload,
