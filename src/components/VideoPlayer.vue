6f0b6be14fbf4e60201b00d4f0c898bbaba9f267
alphardex <2582347430@qq.com>
2021-02-05 08:57:04 +0800

    update

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,27 @@
+<template>
+  <video
+    :src="src"
+    :poster="poster"
+    controls
+    playsinline
+    webkit-playsinline
+    x5-video-player-type="h5-page"
+    controlslist="nodownload"
+    class="bg-black"
+  ></video>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "VideoPlayer",
+  props: {
+    src: String,
+    poster: {
+      type: String,
+      default: "",
+    },
+  },
+});
+</script>
