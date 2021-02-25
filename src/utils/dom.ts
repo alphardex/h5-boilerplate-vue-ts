3759609c07272a54c45e3253f84fc175f5e6bd03
alphardex <2582347430@qq.com>
2021-02-25 12:58:58 +0800

    update

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,3 +1,6 @@
+import ky from "kyouka";
+import { compressImage } from "./image";
+
 const preloadAudios = () => {
   const audios = document.querySelectorAll("audio:not(.bgm)");
   audios.forEach((audio: any) => {
@@ -11,4 +14,18 @@ const preloadAudios = () => {
     );
   });
 };
-export { preloadAudios };
+
+const compressAndUploadMultipleImages = (e: Event, cb: Function) => {
+  const files = (e.target as HTMLInputElement).files;
+  if (files) {
+    Array.from(files).forEach((file: any) => {
+      compressImage(file, (compressed: Blob) => {
+        ky.loadImageAsBase64URL(compressed, (base64URL: string) => {
+          cb(base64URL);
+        });
+      });
+    });
+  }
+};
+
+export { preloadAudios, compressAndUploadMultipleImages };
