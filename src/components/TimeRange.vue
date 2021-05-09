1352e0e2a704ae587fb2b66cb36eff92d2b2b392
alphardex <2582347430@qq.com>
2021-05-09 09:57:38 +0800

    update

---
+++ src/components/TimeRange.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <span>活动时间：</span>
+    <span>{{ timeRangeText }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { formatDate } from "@/utils/dom";
+import { computed, defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TimeRange",
+  props: {
+    info: {
+      type: Object,
+      default: null,
+    },
+  },
+  setup(props) {
+    const timeRangeText = computed(() => {
+      return props.info
+        ? `${formatDate(props.info.startdate)}-${formatDate(
+            props.info.enddate
+          )}`
+        : "";
+    });
+    return { timeRangeText };
+  },
+});
+</script>
